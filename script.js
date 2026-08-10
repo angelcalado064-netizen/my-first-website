a99291c54aab054e18b5d778b1c0560521d7465c
angelcalado064-netizen <angelcalado064@gmail.com>
2026-08-10 11:56:02 +0800

    Implement PIN entry and unlock functionality

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+let pin = "";
 
+const correctPin = "081824";
+
+function press(number) {
+    if (pin.length < 6) {
+        pin += number;
+        updateDots();
+    }
+}
+
+function clearPin() {
+    pin = "";
+    updateDots();
+
+    document.getElementById("message").innerHTML = "";
+}
+
+function updateDots() {
+    for (let i = 1; i <= 6; i++) {
+        const dot = document.getElementById("dot" + i);
+
+        if (i <= pin.length) {
+            dot.classList.add("filled");
+        } else {
+            dot.classList.remove("filled");
+        }
+    }
+}
+
+function unlock() {
+    const message = document.getElementById("message");
+
+    if (pin === correctPin) {
+
+        message.className = "message correct";
+        message.innerHTML = "You remembered. ♡";
+
+    } else {
+
+        message.className = "message wrong";
+        message.innerHTML = "Hmm... try again. ♡";
+
+        setTimeout(function() {
+            pin = "";
+            updateDots();
+        }, 800);
+    }
+}
